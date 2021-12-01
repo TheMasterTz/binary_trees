ba4f6bbaea3b173753c1a11ff1bd06b6df11f495
SebasVillaLo <villalopezsebastian@gmail.com>
2021-12-01 15:50:15 -0500

    add proyect

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -1,6 +1,6 @@
 #include "binary_trees.h"
 /**
- * binary_tree_insert_left - function that inserts a node as the left-child of another node
+ * binary_tree_insert_left - function that inserts a node as the left-child of
  *
  * @parent: Pointer to the parent node
  * @value: value is the value to put in the new node
--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -1,6 +1,7 @@
 #include "binary_trees.h"
 /**
- * binary_tree_insert_right - function that inserts a node as the right-child of another node
+ * binary_tree_insert_right - function that inserts a node as the right-child
+ * of another node
  *
  * @parent: pointer to the node to insert the right-child in
  * @value: value is the value to put in the new node
